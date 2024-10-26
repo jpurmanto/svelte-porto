d59562e1062bac835ad6ad3ed2c9417fe4ed3d35
rafia9005 <rafia9005@gmail.com>
2024-10-26 21:34:10 +0700

    ui(about me)

--- src/components/layouts/AboutLayout.svelte
+++ src/components/layouts/AboutLayout.svelte
@@ -1,5 +1,21 @@
-<div class="flex justify-center items-center h-screen bg-tokyo-night-background">
-  <div>
-  <h1>hello world</h1>
+<div class="flex justify-center items-center bg-tokyo-night-background py-2">
+  <div class="text-center w-full max-w-2xl px-4">
+    <div class="flex justify-center items-center mb-6">
+      <img
+        src="./profile.jpg"
+        alt="Ahmad Rafi'i Profile Image"
+        class="w-[100px] h-[100px] rounded-full"
+      />
+    </div>
+    <h1 class="font-bold text-2xl text-[#7aa2ff] mt-2">About Me</h1>
+    <p class="mt-4 text-sm text-[#b3b6b9]">
+      Hi! I'm <strong class="text-tokyo-night-cyan">Ahmad Rafi'i</strong>, a
+      Full-Stack Developer passionate about creating modern web applications.
+    </p>
+    <div class="mt-4 text-tokyo-night-foreground mx-auto max-w-[90%] md:max-w-[50%] sm:w-1/2 w-auto"> 
+      <p class="text-sm md:text-base">
+        I am a passionate <strong>Full Stack Developer</strong> with two years of experience specializing in web development. I have a strong foundation in a variety of technologies, including Svelte, NestJS, Next.js, Vue.js, Laravel, and Express.js. Currently, I am pursuing my studies at <strong>SMKN 6 Malang</strong> in the <strong>Information Systems and Applications Network</strong> (SIJA) program.
+      </p>
+    </div>
   </div>
 </div>
